71532143b7bd10d1934431a20bb37a33e3984ce4
alexanderscher <alexcscher@gmail.com>
2024-09-09 14:17:42 -0700

    update

--- tf/cron/eventbridge.tf
+++ tf/cron/eventbridge.tf
@@ -11,7 +11,7 @@ resource "aws_scheduler_schedule" "store_turn_invoke_schedule" {
   # cron(mins hour day month ? year)
   # cron(05 21 7 9 ? 2024)
   target {
-    arn      = aws_lambda_function.store_turn_invoke.arn
+    arn      = "arn:aws:lambda:us-east-1:742736545134:function:store-turn-invoke"
     role_arn = aws_iam_role.store_turn_scheduler_role.arn
   }
 }
