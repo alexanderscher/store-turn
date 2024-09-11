0f05af0ea68b92e273a33a03d341789e884aabf1
alexanderscher <alexcscher@gmail.com>
2024-09-10 17:18:21 -0700

    update

--- tf/cron/eventbridge.tf
+++ tf/cron/eventbridge.tf
@@ -33,3 +33,21 @@
 #     role_arn = aws_iam_role.store_turn_scheduler_role.arn
 #   }
 # }
+
+
+resource "aws_scheduler_schedule" "store_turn_invoke_schedule_1" {
+  name       = "store-turn-invoke-schedule-1"
+  group_name = "default"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression          = "cron(30 17 10 9 ? 2024)"
+  schedule_expression_timezone = "America/Los_Angeles"
+
+  target {
+    arn      = "arn:aws:lambda:us-east-1:742736545134:function:store-turn-invoke"
+    role_arn = aws_iam_role.store_turn_scheduler_role.arn
+  }
+}
