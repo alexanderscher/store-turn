e52a2b32eec3381160affd8ba69b91e4255d1ca2
alexanderscher <alexcscher@gmail.com>
2024-09-19 17:45:47 +0100

    update

--- apple/main.py
+++ apple/main.py
@@ -495,8 +495,10 @@ def lambda_handler(event, context) -> Dict[str, Union[int, str]]:
     service = webdriver.ChromeService("/opt/chromedriver")
 
     # local
+
     # from selenium.webdriver.chrome.service import Service
     # from webdriver_manager.chrome import ChromeDriverManager
+
     # service = Service(ChromeDriverManager().install())
 
     driver = webdriver.Chrome(service=service, options=options)
